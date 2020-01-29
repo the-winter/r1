c3b9943502ead82802322465692d4dabc3a30d0f
thewinter <19483860+the-winter@users.noreply.github.com>
2020-01-29 20:25:16 +0800

    intentions working

--- src/test/test2.js
+++ src/test/test2.js
@@ -3,18 +3,12 @@ import { Text } from './text'
 import { Image } from './image'
 import { PullQuote } from './pullquote'
 import testData from '../data/test2-data.json'
+import ReactHtmlParser from 'react-html-parser';
 import moment from 'moment'
 
 class TestTwo extends Component {
 
     render() {
-        const emphasised = {
-            fontStyle: 'italic'
-        };
-
-        const important = {
-            fontWeight: 'bold'
-        }
 
         // Filter function for an object
         Object.filter = (obj, predicate) =>
@@ -38,33 +32,35 @@ class TestTwo extends Component {
                 </div>
                 {pageData.blocks.map((item, index) => {
                     if (item.kind === "text") {
+                        let intentionsString = '';
                         // check the intentions array is not empty
                         if (item.intentions.length > 0) {
-                            let intentionsString = '';
                             // object with index and length as property e.g. emphasised: [3, 7]
                             const intentions = item.intentions.reduce((o, item) => ({ ...o, [item.kind]: [item.index, item.length] }), {})
                             // each individual character in the text
-                            for (let i = 0; i <= item.text.length; i++) {
+                            for (let i = 0; i < item.text.length; i++) {
 
                                 var char = item.text[i]
                                 // check if i is within the range for any intention
                                 var relevantIntentions = Object.filter(intentions, (value) => {
-                                    var minValue = Math.min(...value);
-                                    var maxValue = Math.max(...value);
+                                    var minValue = value[0]
+                                    var maxValue = value[0] + value[1]
                                     // return the intention
                                     return (i >= minValue) && (i < maxValue)
                                 })
                                 // get the intention name as a string
                                 var styles = Object.keys(relevantIntentions).join(" ")
                                 // put the intention name as a style on the span element
-                                intentionsString += `<span className=${styles}>${char}</span>`
+                                intentionsString += `<span class="${styles}">${char}</span>`
                             }
                         } else {
                             // if no intentions return only text
                             return item.text
                         }
+                        return ReactHtmlParser(intentionsString)
 
-                        return <Text text={item.text} intentions={item.intentions} key={index} />
+                        // return <Text text={item.text} intentions={item.intentions} key={index} />
+
                     } else if (item.kind === "image") {
                         return <Image captionText={item.captionText} url={item.url} key={index} />
 
